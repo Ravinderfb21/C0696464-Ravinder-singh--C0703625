c5c615830782f7e9cf9ca7e3727a03e83592e4e6
visualstudio <visualstudio@230-PC18>
2019-03-14 15:25:37 -0400

    UnitTestmethod2- method1 testing

--- BankAccount.Tester/UnitTest1.cs
+++ BankAccount.Tester/UnitTest1.cs
@@ -23,5 +23,21 @@ namespace BankAccount.Tester
             double actual = account.Balance;
             Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            //Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = -100.00;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            //Act
+            account.Debit(debitAmount);
+
+            //assert is handled by the expectedException attribute on the test method.
+}
+
     }
 }
